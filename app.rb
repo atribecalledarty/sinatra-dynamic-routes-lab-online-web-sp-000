43e089277f462959a507b7025c403bb32595b814
Arthur Song <arth3rs0ng@gmail.com>
2019-05-13 20:54:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,6 +14,7 @@ class App < Sinatra::Base
   
   get '/say/:number/:phrase' do
     counter = 0
+    "what the heck"
     while (counter < params[:number].to_i) do
       "what the heck"
       "#{params[:phrase]}"
