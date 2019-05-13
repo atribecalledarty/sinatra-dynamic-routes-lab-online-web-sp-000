4eee32badc1594b3589f1920d9f1f19805f29a0f
Arthur Song <arth3rs0ng@gmail.com>
2019-05-13 20:59:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
   
   get '/say/:number/:phrase' do
-    "#{params[:number].to_i}"
+    params[:number].to_i.times {}
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
