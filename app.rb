59d56f2905d563339d39ea012feb5d6421def147
Arthur Song <arth3rs0ng@gmail.com>
2019-05-13 20:59:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,9 +13,7 @@ class App < Sinatra::Base
   end
   
   get '/say/:number/:phrase' do
-    params[:number].to_i.times do
-      "whatsup"
-    end
+    params[:number]
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
